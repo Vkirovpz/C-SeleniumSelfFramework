d2928513e798948a6a4e66c3a40eb4318228f4c4
Vkirovpz <Vkirovpz@students.softuni.bg>
2024-09-16 15:56:42 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace C_SeleniumSelfFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
